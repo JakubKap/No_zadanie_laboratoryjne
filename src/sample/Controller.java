18084f3de5f5cab3acc838d232048135f3a7d397
Jakub Kap <jakub1996k@gmail.com>
2021-01-04 17:33:46 +0100

    Added fx:ids and mapping in controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField accuracyTextArea;
+
+    @FXML
+    private Button fileButton;
+
+    @FXML
+    private Label selectedFileLabel;
+
+    @FXML
+    private Button acceptParametersButton;
 }
