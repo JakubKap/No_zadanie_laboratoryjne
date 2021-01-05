4c18a426a022f0f8cf804cf5493d37c48c5b9e34
Jakub Kap <jakub1996k@gmail.com>
2021-01-05 17:21:55 +0100

    Finished Schick-Wolverton model

--- src/Controller.java
+++ src/Controller.java
@@ -4,6 +4,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import models.JelinskiMoranda;
+import models.SchickWolverton;
 import utilies.CsvReader;
 
 public class Controller {
@@ -48,6 +49,19 @@ public class Controller {
         calcResultTextArea.appendText("\nWyznaczona wartość oczekiwana czasu, ");
         calcResultTextArea.appendText("\njaki upłynie do momentu wykrycia " +
                 jelinskiMoranda.getNEXT_FAULT_TIME() + " błędu: " + jelinskiMoranda.getEt());
+
+        SchickWolverton schickWolverton = new SchickWolverton(Double.parseDouble(accuracyTextField.getText()),
+                csvReader.getFaultTimesArray());
+        schickWolverton.estimateBigNAndFi();
+
+        calcResultTextArea.appendText("\n\nMetoda Schicka-Wolvertona:");
+        calcResultTextArea.appendText("\n\nWybrana dokładność: " + accuracyTextField.getText());
+        calcResultTextArea.appendText("\nLiczba dotychczas wykrytych błędów: " + schickWolverton.getSmallN());
+        calcResultTextArea.appendText("\nWyznaczony parametr N: " + schickWolverton.getBigN());
+        calcResultTextArea.appendText("\nWyznaczony parametr fi: " + schickWolverton.getFi());
+        calcResultTextArea.appendText("\nWyznaczona wartość oczekiwana czasu, ");
+        calcResultTextArea.appendText("\njaki upłynie do momentu wykrycia " +
+                schickWolverton.getNEXT_FAULT_TIME() + " błędu: " + schickWolverton.getEt());
 
     }
 }
---
+++ src/models/SchickWolverton.java
@@ -0,0 +1,95 @@
+package models;
+
+public class SchickWolverton {
+    private double accuracy;
+    private Integer[] faultTimesArray;
+    private int smallN;
+    private int bigN;
+    private double fi;
+    private double et;
+    private final int NEXT_FAULT_TIME = 241;
+
+    public SchickWolverton(double accuracy, Integer[] faultTimesArray) {
+        this.accuracy = accuracy;
+        this.faultTimesArray = faultTimesArray;
+        this.smallN = this.faultTimesArray.length;
+        this.bigN = 242;
+        this.fi = 0;
+        //0.001
+    }
+
+    public void estimateBigNAndFi(){
+        double leftSide;
+        double rightSide;
+
+        do {
+            leftSide = countLeftSide();
+            rightSide = countRightSide();
+
+            System.out.println("N = " + bigN);
+            System.out.println("leftSide = " + leftSide);
+            System.out.println("rightSide = " + rightSide);
+            System.out.println("fault =" + Math.abs(leftSide - rightSide));
+            boolean cond = Math.abs(leftSide - rightSide) > accuracy;
+            System.out.println("condition = " + cond);
+
+            bigN++;
+        }
+
+        while(Math.abs(leftSide - rightSide) > accuracy);
+
+        bigN = bigN - 1;
+        et = Math.sqrt(Math.PI / (2*fi*(bigN - NEXT_FAULT_TIME)));
+
+        System.out.println("final N = " + bigN);
+    }
+
+    private double countLeftSide(){
+        fi = 0;
+
+        for(int i=1; i <= smallN; i++)
+            fi += 1 / (((double) bigN - (i - 1)) * sumOfFaultTimesArraySquareElements());
+
+        fi = 2*fi;
+        return 2*fi;
+    }
+
+    private double countRightSide(){
+        double result = 2 * smallN;
+
+        double denSecPart = 0;
+        for (int i=0; i<smallN; i++)
+            denSecPart += i * faultTimesArray[i] * faultTimesArray[i];
+
+        return result/(sumOfFaultTimesArraySquareElements() * bigN - denSecPart);
+    }
+
+    private double sumOfFaultTimesArraySquareElements(){
+        double sum = 0;
+
+        for (Integer value : this.faultTimesArray)
+            sum += value*value;
+
+        return sum;
+    }
+
+    public int getSmallN() {
+        return smallN;
+    }
+
+    public int getBigN() {
+        return bigN;
+    }
+
+    public double getFi() {
+        return fi;
+    }
+
+    public double getEt() {
+        return et;
+    }
+
+    public int getNEXT_FAULT_TIME() {
+        return NEXT_FAULT_TIME;
+    }
+}
